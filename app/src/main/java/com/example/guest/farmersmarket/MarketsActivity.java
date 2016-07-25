3eebc2d013052fa0da63e2914eaba052c2298133
5adiyah <sadiali@pdx.edu>
2016-07-25 11:20:49 -0700

    Add markets activity

---
+++ app/src/main/java/com/example/guest/farmersmarket/MarketsActivity.java
@@ -0,0 +1,13 @@
+package com.example.guest.farmersmarket;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MarketsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_markets);
+    }
+}
