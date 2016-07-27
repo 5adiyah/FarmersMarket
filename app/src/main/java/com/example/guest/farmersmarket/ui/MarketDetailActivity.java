b999575aebfe2d38654bd22b341046966707661c
5adiyah <sadiali@pdx.edu>
2016-07-27 15:45:39 -0700

    Add market details

--- app/src/main/java/com/example/guest/farmersmarket/ui/MarketDetailActivity.java
+++ app/src/main/java/com/example/guest/farmersmarket/ui/MarketDetailActivity.java
@@ -3,17 +3,26 @@ package com.example.guest.farmersmarket.ui;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.example.guest.farmersmarket.MarketDetails;
 import com.example.guest.farmersmarket.R;
 import com.example.guest.farmersmarket.adapters.MarketPagerAdapter;
 import com.example.guest.farmersmarket.models.Market;
+import com.example.guest.farmersmarket.service.MarketService;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.parceler.Parcels;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class MarketDetailActivity extends AppCompatActivity {
     @Bind(R.id.viewPager)
@@ -30,9 +39,16 @@ public class MarketDetailActivity extends AppCompatActivity {
 
         mMarkets = Parcels.unwrap(getIntent().getParcelableExtra("marketName"));
         int startingPosition = getIntent().getIntExtra("position", 0);
+        Log.d("the id", String.valueOf(mMarkets.get(startingPosition).getId()));
+
 
         adapterViewPager = new MarketPagerAdapter(getSupportFragmentManager(), mMarkets);
         mViewPager.setAdapter(adapterViewPager);
         mViewPager.setCurrentItem(startingPosition);
+
     }
+
+
+    //API GOES HERE!
+
 }
--- app/src/main/java/com/example/guest/farmersmarket/ui/MarketDetailFragment.java
+++ app/src/main/java/com/example/guest/farmersmarket/ui/MarketDetailFragment.java
@@ -3,32 +3,48 @@ package com.example.guest.farmersmarket.ui;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.guest.farmersmarket.MarketDetails;
 import com.example.guest.farmersmarket.R;
 import com.example.guest.farmersmarket.models.Market;
+import com.example.guest.farmersmarket.service.MarketService;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.parceler.Parcels;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MarketDetailFragment extends Fragment {
-    @Bind(R.id.marketNameTextView)
-    TextView mMarketNameTextView;
+    @Bind(R.id.marketNameTextView) TextView mMarketNameTextView;
+    @Bind(R.id.marketScheduleTextView) TextView mMarketScheduleTextView;
+    @Bind(R.id.marketAddressTextView) TextView mMarketAddressTextView;
+    @Bind(R.id.marketProductsTextView) TextView mMarketProductsTextView;
 
     private Market mMarket;
+
+    private ArrayList<MarketDetails> mMarketDetailsArray = new ArrayList<MarketDetails>();
 
     public static MarketDetailFragment newInstance(Market market) {
         MarketDetailFragment marketDetailFragment = new MarketDetailFragment();
         Bundle args = new Bundle();
         args.putParcelable("marketName", Parcels.wrap(market));
+
         marketDetailFragment.setArguments(args);
         return marketDetailFragment;
     }
@@ -37,6 +53,7 @@ public class MarketDetailFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mMarket = Parcels.unwrap(getArguments().getParcelable("marketName"));
+        getMarketDetails(mMarket);
     }
 
     @Override
@@ -44,10 +61,69 @@ public class MarketDetailFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_market_detail, container, false);
         ButterKnife.bind(this, view);
+        Log.d("array up", String.valueOf(mMarketDetailsArray.size()));
 
-        mMarketNameTextView.setText(mMarket.getMarketName());
+//        while(mMarketDetailsArray.size() >= 1){
+//            if(mMarketDetailsArray.size() > 0){
+//
+//                System.out.println("hello" + mMarketAddressTextView.getText().toString());
+//                break;
+//            }
+//        }
+
+        mMarketNameTextView.setText(mMarket.getMarketName()); //this changes the title
 
         return view;
+    }
+
+    private void getMarketDetails(Market market) {
+        final MarketService marketService = new MarketService();
+        marketService.findMarketDetails(market, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    String jsonData = response.body().string();
+                    if (response.isSuccessful()) {
+                        JSONObject marketJson = new JSONObject(jsonData);
+                        JSONObject marketDetailsJson = marketJson.getJSONObject("marketdetails");
+
+                        String oldAddress = marketDetailsJson.getString("Address");
+//                        int parenthesisIndex1 = oldAddress.indexOf("(");
+//                        int parenthesisIndex2 = oldAddress.indexOf(")");
+//                        String replace = oldAddress.substring(parenthesisIndex1 -1, parenthesisIndex2 + 1);
+//                        String address = oldAddress.replace(replace, "");
+
+                        String products = marketDetailsJson.getString("Products");
+                        String schedule = marketDetailsJson.getString("Schedule");
+                        final MarketDetails marketDetails = new MarketDetails(oldAddress, products, schedule);
+                        Log.d("marketDetails address", marketDetails.getAddress());
+
+//                        mMarketDetailsArray.add(marketDetails);
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                mMarketAddressTextView.setText(marketDetails.getAddress());
+                                mMarketScheduleTextView.setText(marketDetails.getSchedule());
+                                mMarketProductsTextView.setText(marketDetails.getProducts());
+                            }
+                        });
+
+//                        Log.d("array", String.valueOf(mMarketDetailsArray.size()));
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                } catch (StringIndexOutOfBoundsException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
 }
